bad55dbf4fb82e04f5a28b8fd9159f5406bdf8fe
karmachevas <karmachev.as@yandex.ru>
2023-03-18 19:43:43 +0500

    Add Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string str = (Console.ReadLine()!);
+while (str.Length < 5 || str.Length > 5)
+{
+    Console.Write("Вы ошиблсь\nВведите пятизначное число: ");
+    str = (Console.ReadLine()!);
+}
+if (Convert.ToInt32(str[0].ToString()) == Convert.ToInt32(str[4].ToString()) && Convert.ToInt32(str[1].ToString()) == Convert.ToInt32(str[3].ToString()))
+    Console.Write("Да");
+else 
+    Console.Write("Нет");
