bd237e4109515070c01f8b53121a421b3b8e8802
karmachevas <karmachev.as@yandex.ru>
2023-03-20 22:59:31 +0500

    Add Task 25

---
+++ Task25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите число A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите натуральное число B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+while (b < 1) 
+{
+    Console.Write("Вы ошиблись\nВведите натуральное число B: ");
+    b = Convert.ToInt32(Console.ReadLine());
+}
+int result = 1;
+for (int i = 0; i < b; i++)
+{
+    result = result * a;
+}
+Console.WriteLine(result);
