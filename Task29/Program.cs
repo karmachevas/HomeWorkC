25e2df630ae74f6c7b941c1a2d70f9be4299c063
karmachevas <karmachev.as@yandex.ru>
2023-03-20 23:18:12 +0500

    Add Task 29

---
+++ Task29/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+int[] mass = new int[8];
+
+void FillArray(int[] array, int a, int b)
+{
+    int lenght = array.Length;
+    for (int index = 0; index < lenght; index++)
+    {
+        array[index] = new Random().Next(a, b + 1);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int index = 0;
+    while (index < array.Length)
+    {
+        if (index == 0)
+            Console.Write($"[{array[index]}");
+        else if (index == array.Length - 1)
+            Console.Write($", {array[index]}]");
+        else Console.Write($", {array[index]}");
+        index++;
+    }
+}
+
+FillArray(mass, 1, 100);
+PrintArray(mass);
