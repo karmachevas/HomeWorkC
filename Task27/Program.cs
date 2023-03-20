1c6bdb21e57b14c250c0af1caff7692ce82a02a2
karmachevas <karmachev.as@yandex.ru>
2023-03-20 23:06:15 +0500

    Add Task 27

---
+++ Task27/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(n);
+int result = 0;
+for (int i = 0; i < str.Length; i++)
+{
+    result = result + Convert.ToInt32(str[i].ToString());
+}
+Console.WriteLine(result);
