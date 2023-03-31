c7e3e9011808784d089b64497358505c944cf41e
karmachevas <karmachev.as@yandex.ru>
2023-03-31 19:59:56 +0500

    Add Task 41 and Task43

---
+++ Task41/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int M = 5;
+int[] arr = new int [M];
+int count = 0;
+Console.WriteLine($"Необходимо ввести {M} чисел");
+for (int i = 0; i < M; i++)
+{
+    Console.WriteLine($"Введите {i+1}-ое число: ");
+    arr[i] = int.Parse(Console.ReadLine()!);
+    if (arr[i] > 0)
+        count++;
+}
+Console.WriteLine($"[{string.Join(", ", arr)}]");
+Console.WriteLine(count);
---
+++ Task43/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear();
+Console.Write("Введите b1: ");
+double b1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите k1: ");
+double k1 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите b2: ");
+double b2 = double.Parse(Console.ReadLine()!);
+Console.Write("Введите k2: ");
+double k2 = double.Parse(Console.ReadLine()!);
+double x, y;
+x= (b2-b1)/(-k2+k1);
+y= k1*((b2-b1)/(-k2+k1))+b1;
+
+Console.WriteLine($"X: {x}, Y: {y}");
