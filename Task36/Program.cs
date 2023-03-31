7af54aa3ade4fc83a56779d7dfe26b8f005e9206
karmachevas <karmachev.as@yandex.ru>
2023-03-31 14:46:27 +0500

    add Task 36

---
+++ Task36/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+
+Console.Clear();
+int[] mass = new int[8];
+
+void FillArray(int[] array, int a, int b)
+{
+    int lenght = array.Length;
+    for (int index = 0; index < lenght; index++)
+    {
+        array[index] = new Random().Next(a, b + 1);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine($"{array[array.Length - 1]}]");
+}
+
+void PrintSummElemNotEvenPosArray(int[] array)
+{
+    int summ = 0;
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        if (i % 2 == 0)
+        {
+            summ = summ + array[i];
+        }
+    }
+    Console.WriteLine(summ);
+}
+
+FillArray(mass, -10, 10);
+PrintArray(mass);
+PrintSummElemNotEvenPosArray(mass);
