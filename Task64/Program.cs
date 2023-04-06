32e6243adfe77f43a6fe97e6e1ea942367b33139
karmachevas <karmachev.as@yandex.ru>
2023-04-06 22:38:40 +0500

    Add Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,12 @@
+﻿string f(int n)
+{
+    if (n == 1)
+        return $"{1}";
+    else 
+        return $"{n} " + f(n - 1);
+}
+
+Console.Clear();
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write(f(n));
