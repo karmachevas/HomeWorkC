c3a7e5d173c3936132799daaa3cbc161a890c4cb
karmachevas <karmachev.as@yandex.ru>
2023-04-06 22:46:49 +0500

    Add Task66

---
+++ Task66/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число M: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число N: ");
+int n = int.Parse(Console.ReadLine()!);
+
+int sum = 0;
+for (int i = m; i <= n; i++)
+{
+    sum += i;
+}
+
+Console.WriteLine($"Результат: {sum}");
